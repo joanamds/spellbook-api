e58af0d527794635246037cdf377688e9e3f3d46
joanamds <joanamdsantos1@gmail.com>
2023-09-26 18:13:00 -0300

    :hammer: refactor: refatorando a api

--- src/seeders/20230926191245-users.js
+++ src/seeders/20230926191245-users.js
@@ -2,49 +2,62 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('students', [
+    await queryInterface.bulkInsert('users', [
       {
-        student_name: 'Harry Potter',
+        user_name: 'Harry Potter',
         house: 'Gryffindor',
         email: 'harry@hogwarts.email.com',
         password: 'mischiefmanaged',
+        role: 'student'
       },
       {
-        student_name: 'Hermione Granger',
+        user_name: 'Hermione Granger',
         house: 'Gryffindor',
         email: 'hermione@hogwarts.email.com',
         password: 'wingardiumleviosa',
+        role: 'student'
       },
       {
-        student_name: 'Ron Weasley',
+        user_name: 'Ron Weasley',
         house: 'Gryffindor',
         email: 'ron@hogwarts.email.com',
         password: 'chocofrog',
+        role: 'student'
       },
       {
-        student_name: 'Draco Malfoy',
+        user_name: 'Draco Malfoy',
         house: 'Slytherin',
         email: 'draco@hogwarts.email.com',
         password: 'ihatemudbloods',
+        role: 'student'
       },
       {
-        student_name: 'Luna Lovegood',
+        user_name: 'Luna Lovegood',
         house: 'Ravenclaw',
         email: 'luna@hogwarts.email.com',
         password: 'nargles!',
+        role: 'student'
       },
       {
-        student_name: 'Cedric Diggory',
+        user_name: 'Cedric Diggory',
         house: 'Hufflepuff',
         email: 'cedric@hogwarts.email.com',
         password: 'triwizardchamp',
+        role: 'student'
       },
-      // Add more students as needed
+      {
+        user_name: 'Albus Percival Wulfric Brian Dumbledore',
+        house: 'Gryffindor',
+        email: 'albuspwb.dumbledore@hogwarts.email.com',
+        password: 'phoenixfeather',
+        role: 'admin'
+      }
+      // Add more users as needed
     ], { timestamps: false});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('students', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   },
 };
 
--- src/seeders/20230926193435-spells.js
+++ src/seeders/20230926193435-spells.js
@@ -3,7 +3,6 @@ module.exports = {
     await queryInterface.bulkInsert('spells', [
       {
         id: 1,
-        admin_id: 1,
         spell_name: 'Expelliarmus',
         description: 'A disarming charm that forces the target to release whatever they are holding.',
         incantation: 'Expelliarmus!',
@@ -11,7 +10,6 @@ module.exports = {
       },
       {
         id: 2,
-        admin_id: 1,
         spell_name: 'Lumos',
         description: 'A charm that produces light from the tip of the caster\'s wand.',
         incantation: 'Lumos!',
@@ -19,7 +17,6 @@ module.exports = {
       },
       {
         id: 3,
-        admin_id: 1,
         spell_name: 'Alohomora',
         description: 'A spell used to unlock doors and objects.',
         incantation: 'Alohomora!',
@@ -27,7 +24,6 @@ module.exports = {
       },
       {
         id: 4,
-        admin_id: 1,
         spell_name: 'Expecto Patronum',
         description: 'A defensive charm that conjures a Patronus to ward off Dementors and other dark creatures.',
         incantation: 'Expecto Patronum!',
